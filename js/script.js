f2d00cf26e130da8f6841a4eeb7092dbcf44bdfe
JosAAV <jose_antonio_alvino21@outlook.es>
2018-12-12 11:18:05 -0500

    Primeros ejemplos de POO con js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Declarando objetos
+
+//Cliente de EDteam
+/*
+const clienteEDteam =
+{
+    nombre : "Beto",
+    apellido:"Quiroga",
+    email:"beto@ed.team",
+    pass : "ASFASDF%asdf23aasd"
+}
+
+//Accediendo a un atributo
+// sintaxis punto
+
+console.log(clienteEDteam.nombre)
+
+//sintaxis corchete
+
+console.log(clienteEDteam["apellido"])
+*/
+
+// --------------------------------------------------------------------------------
+/// Haciendo uso de Template String
+/*
+let na = "nom";
+let me = "bre";
+
+const tabla =
+{
+    [`${na}${me}`] : "Beto"
+}
+
+console.log(tabla)
+*/
+
+// --------------------------------------------------------------------------------
+
